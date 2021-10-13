61bd19d88274bde19f9fdb6ea2ffd0883c55ef75
Tendo <71673094+JeffOfTheSixPaths@users.noreply.github.com>
2021-10-13 10:16:48 -0500

    only for testing functions

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from Scrap import *
 import json
-f = open('imp.json')
+f = open('websites.json')
 data = json.load(f)
 headline_list = get_page(data["yahoo finance"]["url"]).find_all("h3")
 summ = get_summaries(data["yahoo finance"]["url"])
