51621f1b1b7b0ba38e759168c1c9d561ff23627b
Tendo <71673094+JeffOfTheSixPaths@users.noreply.github.com>
2021-10-14 13:00:06 -0500

    Updated getting summaries
    made it search for the first div with class="caas-body" and then remove all the angle brackets within that

--- Scrap.py
+++ Scrap.py
@@ -19,7 +19,7 @@ def find_href(string):
 	
 	
 def get_summaries(site):
-	return get_page(str(site)).find_all("p")
+	return rmAngles(str(get_page(str(site)).find_all("div", class_="caas-body")[1].txt))
 
 def get_articles(site):
 	article = get_summaries(str(site))
